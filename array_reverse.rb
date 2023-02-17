af1fa00c8e0fab381ea3fae5eceb311fed00b634
Ben Purinton <ben.purinton@gmail.com>
2023-02-17 14:16:40 -0900

    add note about p vs puts

--- array_reverse.rb
+++ array_reverse.rb
@@ -2,6 +2,8 @@
 # Expected output: 
 #   "Enter at least 2 values, separated by spaces:"
 #   ["second", "first", 0]
+#
+# NOTE: use `p` to print the reversed array, do not use `puts`
 
 p "Enter at least 2 values, separated by spaces:"
 
