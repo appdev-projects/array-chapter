39932521860ba82987255d48383d3de154ce98a6
Ben Purinton <ben.purinton@gmail.com>
2023-02-17 14:23:19 -0900

    add note about using provided array

--- array_min_max_difference.rb
+++ array_min_max_difference.rb
@@ -1,9 +1,11 @@
-# Create an Array of numbers and output the number with the lowest value in
+# Using the provided Array of numbers, output the number with the lowest value in
 # the array, the number with the highest value in the array, and the difference
 # between the highest value and the lowest value.
 # 
 #   lowest_number
 #   highest_number
 #   difference
+#
+# NOTE: for this exercise use the provided array (don't ask the user for a value, in this task).
 
 array = [12, 23, 41, 73, 19, 6]
